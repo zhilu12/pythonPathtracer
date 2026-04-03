d80cfad19e269d22b8686a93ebae6acc3a979386
Zhi Lin Lu <zhilinlu12@gmail.com>
2026-04-03 17:06:23 -0400

    added simple ray class

---
+++ ray.py
@@ -0,0 +1,18 @@
+from vec3 import Vec3
+from vec3 import point3
+
+class Ray:
+    def __init__(self, origin, direction):
+        self.orig = origin
+        self.dir = direction
+
+    @property
+    def origin(self):
+        return self.orig
+    
+    @property
+    def direction(self):
+        return self.dir
+    
+    def at(self, t):
+        return self.orig + t * self.dir
