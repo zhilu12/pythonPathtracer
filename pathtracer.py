bf7bfd456df57b5718c098db753f042f52c34389
Zhi Lin Lu <zhilinlu12@gmail.com>
2026-04-06 16:01:06 -0400

    added camera and viewport into pathtracer. Ray_color blending blue based on y level currently

--- pathtracer.py
+++ pathtracer.py
@@ -1,10 +1,59 @@
 from tqdm import tqdm
 from color import color
 from color import write_color
+from ray import *
+from vec3 import *
+
+def ray_color(r):
+    unit_direction = unit_vector(r.direction)
+    a = 0.5 * (unit_direction.y + 1.0)
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0)
+
+
 
 def main():
-    imgWidth = 256
-    imgHeight = 256
+
+    # Image
+    aspectRatio = 16.0/9.0
+    imgWidth = 400
+
+    imgHeight = int(imgWidth/aspectRatio)
+    if imgHeight < 1:
+        imgHeight = 1
+
+
+    # Camera
+    focalLength = 1.0
+    viewportHeight = 2.0
+    viewportWidth = viewportHeight * (float(imgWidth)/imgHeight)
+    camera_center = point3(0, 0, 0)
+
+    # Vectors across horizontal and vertical viewport edges
+    viewport_u = Vec3(viewportWidth, 0, 0)
+    viewport_v = Vec3(0, -viewportHeight, 0)
+
+    # Horizontal and vertical deltas from pixel to pixel
+    pixelDelta_u = viewport_u/imgWidth
+    pixelDelta_v = viewport_v/imgHeight
+
+    # Location of the upper left pixel
+    
+    """
+        Starting from the camera center, we first move center of the 
+        viewport by subtracting the focal length.
+        Then we compute u and v by taking half the viewports
+        Imagine it as viewing the center of the image and getting to top left
+    """
+    viewport_upper_left = (camera_center 
+                           - Vec3(0, 0, focalLength) 
+                           - viewport_u/2 
+                           - viewport_v/2
+                           )
+    
+    pixel00_loc = (viewport_upper_left
+                   + 0.5 * (pixelDelta_u + pixelDelta_v)
+                   )
+    
 
     print("P3")
     print(imgWidth, imgHeight)
@@ -12,7 +61,13 @@ def main():
 
     for j in tqdm(range(imgHeight)):
         for i in (range(imgWidth)):
-            pixel_color = color(float(i) / (imgWidth - 1), float(j)/(imgHeight - 1), 0)
+            pixel_center = pixel00_loc + (i * pixelDelta_u) + (j * pixelDelta_v)
+            ray_direction = pixel_center - camera_center
+            r = Ray(camera_center, ray_direction)
+
+            pixel_color = ray_color(r)
+            
+            # color(float(i) / (imgWidth - 1), float(j)/(imgHeight - 1), 0)
 
             write_color(pixel_color)
 
