8cc05ac650dce94dd680ae25d01ea3ff6ad60383
Zhi Lin Lu <zhilinlu12@gmail.com>
2026-04-13 12:19:33 -0400

    simplified hit_sphere quadratic caculations by substituting b = -2h into the formula. Returns the time t in terms of h, a, c

--- pathtracer.py
+++ pathtracer.py
@@ -5,8 +5,12 @@ from ray import *
 from vec3 import *
 
 def ray_color(r):
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)):
-        return (color(1, 0, 0))
+    t = hit_sphere(point3(0, 0, -1), 0.5, r)
+    if t > 0.0:
+        N = unit_vector(r.at(t) - Vec3(0, 0, -1))
+        return 0.5 * color(N.x + 1, N.y + 1, N.z + 1)
+    
+    
     
     unit_direction = unit_vector(r.direction)
     a = 0.5 * (unit_direction.y + 1.0)
@@ -27,15 +31,24 @@ def hit_sphere(center, radius, r):
     oc = center - r.origin
 
     # using the quadrative formula to get values for a, b, c
-    
-    a = dot(r.direction, r.direction)
+    a = r.direction.length_squared()
     b = -2.0 * dot(r.direction, oc)
-    c = dot(oc, oc) - radius*radius
+    c = oc.length_squared() - radius*radius
+
+
+    # Subbing in b = -2h into the quadratic formula
+    # = (h +- sqrt(h^2 - ac)) / a
+    # and h = d * (C - Q)
+    h = dot(r.direction, oc)
 
     # if discriminant is valid, that means there are possible values 
     # for t in which the ray intersects the sphere
-    discriminant = b*b - 4*a*c
-    return discriminant >= 0
+    discriminant = h*h - a*c
+    
+    if discriminant < 0:
+        return -1.0
+    else:
+        return ((h - math.sqrt(discriminant)) / a)
 
 
 
